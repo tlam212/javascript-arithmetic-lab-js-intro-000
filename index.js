60b7496b17ef3de0aa5149a846a9184fd9b467b3
Toni Lamont <tlam212@gmail.com>
2018-05-11 18:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function add(a,b) {
 function subtract(a, b) {
   return a-b
 }
-function multipy(a, b) {
+function multiply(a, b) {
   return a * b
 }
 function divide(a, b) {
-  return b / a
+  return a / b
 }
