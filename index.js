8a9d9609bcc49a3aadbc0c15003d5c5298a7035f
Toni Lamont <tlam212@gmail.com>
2018-05-11 18:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
-function inc (a) {
+function inc(a) {
   return a++
 }
-function dec (a) {
+function dec(a) {
   return a--
 }
